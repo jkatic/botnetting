fc5bb7004b7f1c5d43cfaf1fa986562d8bbe66b0
Jovan Katic <jovan@vmfarms.com>
2019-12-15 19:19:15 -0500

    Works, but not really

--- slacker/slacker.go
+++ slacker/slacker.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/nlopes/slack"
 )
@@ -30,7 +32,7 @@ Loop:
 				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
 
 				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
-					rtm.SendMessage(rtm.NewOutgoingMessage("What's up buddy!?!?", ev.Channel))
+					respond(rtm, ev, prefix)
 				}
 
 			case *slack.RTMError:
@@ -44,5 +46,38 @@ Loop:
 				//Take no action
 			}
 		}
+	}
+}
+
+func respond(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
+	rand.Seed(time.Now().UnixNano())
+	var response string
+	text := msg.Text
+	text = strings.TrimPrefix(text, prefix)
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+
+	acceptedGreetings := map[string]bool{
+		"what's up?": true,
+		"hey!":       true,
+		"yo":         true,
+	}
+	acceptedHowAreYou := map[string]bool{
+		"how's it going?": true,
+		"how are ya?":     true,
+		"feeling okay?":   true,
+	}
+
+	if acceptedGreetings[text] {
+		if rand.Intn(4) >= 3 {
+			response = "Chookity Pok!"
+			rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+		} else if rand.Intn(4) < 3 {
+			response = "Pookoty Pok Pok!"
+			rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+		}
+	} else if acceptedHowAreYou[text] {
+		response = "Good. How are you?"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
 	}
 }
